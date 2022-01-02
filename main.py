26b2ab28dc8541586c6dd7a2c8c4091553888ffb
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-02 17:08:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Write a program for finding biggest number among 3 numbers
+x=int(input())
+y=int(input())
+z=int(input())
+
+if(x>y and x>z):
+  print(x)
+if(y>x and y>z):
+  print(y)
+if(z>y and z>x):
+  print(z)
